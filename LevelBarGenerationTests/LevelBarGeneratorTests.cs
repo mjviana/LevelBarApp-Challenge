c1ec0a9714f3c2dc310c90d94d7d8e35e515df3f
Mario Viana <mj.viana1993@hotmail.com>
2023-09-27 22:43:15 +0100

    Add unit tests to LevelBarGenerator

---
+++ LevelBarGenerationTests/LevelBarGeneratorTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace LevelBarGeneration.Tests
+{
+    [TestClass()]
+    public class LevelBarGeneratorTests
+    {
+        private bool dataReceived;
+
+        [TestMethod()]
+        public async Task ReceiveDataTest()
+        {
+            var generatorInstance = LevelBarGenerator.Instance;
+
+            generatorInstance.ChannelLevelDataReceived += GeneratorInstance_ChannelLevelDataReceived;
+
+            var connectAction = new Action(async () => await generatorInstance.Connect());
+
+            connectAction.Invoke();
+
+            Assert.IsTrue(dataReceived);
+        }
+
+        private void GeneratorInstance_ChannelLevelDataReceived(object sender, ChannelDataEventArgs e)
+        {
+            dataReceived = e.Levels.Length > 0 && e.ChannelIds.Length > 0;
+        }
+    }
+}
